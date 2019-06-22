fe44763aa40ef62b08dbd94e69b529e076e2dc2e
Burr Sutter <burrsutter@gmail.com>
2019-06-22 13:11:24 -0400

    updating to 1.0.1 with fast/slow

--- quarkus-kafka-knative-consumer/dockerbuild.sh
+++ quarkus-kafka-knative-consumer/dockerbuild.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker build -t burrsutter/quarkus-kafka-knative-consumer:1.0.0 .
+docker build -t burrsutter/quarkus-kafka-knative-consumer:1.0.1 .
--- quarkus-kafka-knative-consumer/src/main/java/com/burrsutter/qkk/HelloResource.java
+++ quarkus-kafka-knative-consumer/src/main/java/com/burrsutter/qkk/HelloResource.java
@@ -10,23 +10,43 @@ import javax.ws.rs.core.MediaType;
 @Path("/")
 public class HelloResource {
 
-    @GET
+    boolean goslow = false;
+
+    @GET  
     @Produces(MediaType.TEXT_PLAIN)
     public String hello() {
         return "hello";
     }
 
+    @GET
+    @Path("/slow")
+    @Produces(MediaType.TEXT_PLAIN)
+    public String slow() {
+        goslow = true;
+        return "slowing down";
+    }
+
+    @GET
+    @Path("/fast")
+    @Produces(MediaType.TEXT_PLAIN)
+    public String fast() {
+        goslow = false;
+        return "Normal speed";
+    }
+
+
     @POST
     @Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.TEXT_PLAIN)
     @Path("/")
-    public String eventGreet(String cloudEventJson) {
-      System.out.println("cloudEventJson: " + cloudEventJson);
-
-      try {
-        Thread.sleep(1000);
-      } catch (Exception e) {
-        System.err.println(e);
+    public String eventGreet(String inputJson) {
+      System.out.println("Received: " + inputJson);
+      if (goslow) {
+        try {
+          Thread.sleep(1000);
+        } catch (Exception e) {
+          System.err.println(e);
+        }
       }
       return "OK";
     }
